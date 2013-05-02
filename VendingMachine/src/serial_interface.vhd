d4c5e6a0f3736d3de29546cce61f164c89879cb7
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-05-02 22:36:29 +0200

    Now serial write is working
    Had to wait until computation was done is "display_10base"

--- VendingMachine/src/serial_interface.vhd
+++ VendingMachine/src/serial_interface.vhd
@@ -16,7 +16,7 @@ entity serial_interface is
 end serial_interface;
 
 architecture Behavioral of serial_interface is
-	type state_type is (state_idle, state_start, state0, state1, state2, state3, state4, state5, state6, state7, state_stop, state_digit_check, state_digit_increment);
+	type state_type is (state_idle, state_wait, state_start, state0, state1, state2, state3, state4, state5, state6, state7, state_stop, state_digit_check, state_digit_increment, state_digit_increment_stop);
 	signal current_state, next_state : state_type;
 	
 	signal cnt, cnt_reg : std_logic_vector(9 downto 0);
@@ -27,8 +27,12 @@ architecture Behavioral of serial_interface is
 	constant CR : std_logic_vector(3 downto 0) := "1101"; -- Carriage return
 	constant LF : std_logic_vector(3 downto 0) := "1010"; -- Line feed
 	
+	signal wait_counter, wait_counter_next : std_logic_vector(19 downto 0);
+	signal wait_enable : std_logic;
+	
 begin
 	digit_next <= digit + 1;
+	wait_counter_next <= wait_counter + 1;
 	
 	process(clk_50,reset)
 	begin
@@ -42,6 +46,11 @@ begin
 			end if;
 			if digit_enable = '1' then
 				digit <= digit_next;
+			end if;
+			if wait_enable = '1' then
+				wait_counter <= wait_counter_next;
+			else
+				wait_counter <= (others => '0');
 			end if;
 		end if;
 	end process;
@@ -57,13 +66,17 @@ begin
 		end if;
 	end process;
 	
-	process(current_state,new_value,digit)
+	process(current_state,new_value,digit,wait_counter)
 	begin
 		next_state <= current_state;		
 		
 		case current_state is
 			when state_idle =>
 				if new_value = '1' then
+					next_state <= state_wait;
+				end if;
+			when state_wait => -- This state is used since we need to wait just a short while until the computation of digit0 and digit1 is done in "display_10base"
+				if wait_counter(19) = '1' then
 					next_state <= state_start;
 				end if;
 			when state_start => next_state <= state0;
@@ -77,26 +90,29 @@ begin
 			when state7 => next_state <= state_stop;
 			when state_stop => next_state <= state_digit_check;
 			when state_digit_check =>
-				if digit = "11" then
-					if new_value = '0' then -- Wait until a new value is set						
-						next_state <= state_digit_increment;
+				if digit = "11" then -- Check if we have send all characters
+					if new_value = '0' then -- Wait until the button is released
+						next_state <= state_digit_increment_stop;
 					end if;
 				else
 					next_state <= state_digit_increment;
 				end if;
-			when state_digit_increment => next_state <= state_idle;
+			when state_digit_increment => next_state <= state_start;
+			when state_digit_increment_stop => next_state <= state_idle;
 			when others => next_state <= state_idle;
 		end case;
 	end process;
 	
-	process(current_state,digit0,digit1,digit) -- The output send the output as 8N1
+	process(current_state,digit,digit0,digit1) -- The output send the output as 8N1
 	begin
 		digit_enable <= '0';
+		wait_enable <= '0';
+		tx <= '1'; -- The start bit sets TX low after that we simply write the byte and set TX high again
 		
-		if current_state = state_idle then
-			tx <= '1';
+		if current_state = state_wait then
+			wait_enable <= '1';
 		elsif current_state = state_start then
-			tx <= '0';
+			tx <= '0'; -- Set TX low to indicate that we want to send a new byte
 		elsif current_state = state0 then
 			if digit = "00" then
 				tx <= digit0(0);
@@ -154,13 +170,11 @@ begin
 		elsif current_state = state7 then
 			tx <= '0';
 		elsif current_state = state_stop then
-			tx <= '1';			
-		--elsif current_state = state_digit_check then
+			tx <= '1'; -- Set it high again after the transfer
 		elsif current_state = state_digit_increment then
 			digit_enable <= '1'; -- This will increment "digit"
-			tx <= '1';
-		else
-			tx <= '1';
+		elsif current_state = state_digit_increment_stop then
+			digit_enable <= '1'; -- This will increment "digit"
 		end if;
 	end process;
 	
